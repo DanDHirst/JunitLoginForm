bd5dc703de1a381c19561a73ca42a93330f14dd8
dhirst1 <dhirst1@DM00058.uopnet.plymouth.ac.uk>
2019-10-28 17:10:58 +0000

    added user class

---
+++ src/loginapp/User.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginapp;
+
+/**
+ *
+ * @author dhirst1
+ */
+public class User {
+    private String name;
+    private String username;
+    private String password;
+    private boolean loggedIn;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    
+    
+    
+}
